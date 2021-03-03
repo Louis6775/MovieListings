a522c727c224d8eb4d18d3f294cd3f59fcbedd5c
louis.ribault <louis.ribault@epita.fr>
2021-03-03 17:47:25 +0000

    first commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieListings.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "this is index";
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            //return "this is welcome";
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View("demo");
+            //return HtmlEncoder.Default.Encode($"Hello {name}, Numtimes is: {numTimes}");
+        }
+    }
+}
---
+++ Models/MvcMoviesContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieListings.Models
+{
+    public class MvcMoviesContext : DbContext
+    {
+        public MvcMoviesContext(DbContextOptions<MvcMoviesContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
